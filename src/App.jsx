f61901fb6506d62f55d41d8a1f5c2d5a4ced26bb
snehaph33 <sneha89213@gmail.com>
2024-10-25 11:17:44 +0530

    si-calculator

---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import TextField from '@mui/material/TextField';
+import './App.css';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+
+function App() {
+  const [principle, setPrinciple] = useState("");
+  const [rate, setRate] = useState("");
+  const [year, setYear] = useState("");
+  const [interest, setInterest] = useState(0);
+  const [isPrincipleInputValid, setIsPrincipleInputValid] = useState(false);
+  const [isYearInputValid, setIsYearInputValid] = useState(false);
+  const [isRateInputValid, setIsRateInputValid] = useState(false);
+
+  const handleValidation = (tag) => {
+    const { name, value } = tag;
+    if (!!value.match(/^\d*\.?\d+$/)) {
+      // valid
+      if (name === "principle") {
+        setPrinciple(value);
+        setIsPrincipleInputValid(false);
+      } else if (name === "year") {
+        setYear(value);
+        setIsYearInputValid(false);
+      } else {
+        setRate(value);
+        setIsRateInputValid(false);
+      }
+    } else {
+      // invalid
+      if (name === "principle") {
+        setPrinciple(value);
+        setIsPrincipleInputValid(true);
+      } else if (name === "year") {
+        setYear(value);
+        setIsYearInputValid(true);
+      } else {
+        setRate(value);
+        setIsRateInputValid(true);
+      }
+    }
+  };
+
+  const handleCalculate = (e) => {
+    e.preventDefault();
+    const principalNum = parseFloat(principle);
+    const rateNum = parseFloat(rate);
+    const yearNum = parseFloat(year);
+
+    if (!isNaN(principalNum) && !isNaN(rateNum) && !isNaN(yearNum)) {
+      setInterest((principalNum * rateNum * yearNum) / 100);
+    }
+  };
+
+  const handleReset = () => {
+    setPrinciple("");
+    setRate("");
+    setYear("");
+    setInterest(0);
+    setIsPrincipleInputValid(false);
+    setIsYearInputValid(false);
+    setIsRateInputValid(false);
+  };
+
+  return (
+    <>
+      <div style={{ minHeight: '100vh', width: '100%' }} className='d-flex justify-content-center align-items-center bg-primary'>
+        <div className="box bg-info p-5 rounded">
+          <h2 className='text-danger fw-bolder' style={{ textDecoration: "underline" }}>Simple Interest Calculator</h2>
+          <p className='text-center'>Calculate your simple interest with us</p>
+          <div className='d-flex justify-content-center align-items-center p-5 rounded bg-warning'>
+            <h1 className='text-danger'>&#8377; {interest}</h1>
+          </div>
+          <div className='mt-5'>
+            <form className='border rounded p-3 d-flex flex-column'>
+              <TextField id="principle" name='principle' value={principle} label="Principle Amount" variant="outlined" onChange={e => handleValidation(e.target)} />
+              {isPrincipleInputValid && <div className='mb-2 text-danger fw-bolder'>*Invalid input</div>}
+
+              <TextField id="year" name='year' value={year} label="Year" variant="filled" onChange={e => handleValidation(e.target)} />
+              {isYearInputValid && <div className='mb-2 text-danger fw-bolder'>*Invalid input</div>}
+
+              <TextField id="rate" name='rate' value={rate} label="Rate of Interest" variant="standard" onChange={e => handleValidation(e.target)} />
+              {isRateInputValid && <div className='mb-2 text-danger fw-bolder'>*Invalid input</div>}
+            </form>
+          </div>
+          <div className="mt-3 d-flex justify-content-between">
+            <Button variant="contained" type='submit' onClick={handleCalculate}>Calculate</Button>
+            <Button variant="outlined" onClick={handleReset}>Reset</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
